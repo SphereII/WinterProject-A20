b7289c9c0a30e92c7603015ef7e4fbf6633efc3a
SphereII <support@7d2dlauncher.org>
2023-04-09 22:04:19 -0300

    Added localized entry for some zombies, disabled VideoPlayers, fixed formatting on music boxes

--- Mods/0-SCore/Properties/AssemblyInfo.cs
+++ Mods/0-SCore/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyVersion("20.0.*")]
 
-[assembly: AssemblyVersion("20.6.464.1520")]
-[assembly: AssemblyFileVersion("20.6.464.1520")]
+[assembly: AssemblyVersion("20.6.465.2133")]
+[assembly: AssemblyFileVersion("20.6.465.2133")]
